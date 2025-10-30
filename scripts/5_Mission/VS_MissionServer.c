c2eb9cd391a527a38e1497103c6491a607e5824b
zediac41 <iamthe_shiznit@yahoo.com>
2025-10-29 21:11:15 -0500

    Create VS_MissionServer.c with initial class structure
    Add initial structure for MissionServer class with comments.

---
+++ scripts/5_Mission/VS_MissionServer.c
@@ -0,0 +1,10 @@
+// We hook into MissionServer so later we can:
+// - validate spawn positions
+// - actually create the vehicle on unpack
+modded class MissionServer
+{
+    // We can add helpers here later, for example:
+    // VS_SpawnVehicleFromData(VS_StoredVehicleData data, vector pos, vector dir)
+
+    // Nothing functional yet in Stage 1
+}
